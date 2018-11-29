c3ff24e36765e9a0df2c4b8546e8cbd8afafd293
Joshua Behrens <code@joshua-behrens.de>
2018-11-29 04:40:21 +0100

    Added support for google binaries

--- ShyimWebP.php
+++ ShyimWebP.php
@@ -1,18 +1,9 @@
 <?php
 
-
 namespace ShyimWebP;
 
-
 use Shopware\Components\Plugin;
-use Shopware\Components\Plugin\Context\InstallContext;
 
 class ShyimWebP extends Plugin
 {
-    public function install(InstallContext $context)
-    {
-        if (!function_exists('imagewebp')) {
-            throw new \RuntimeException('GD is installed without webp support');
-        }
-    }
 }
