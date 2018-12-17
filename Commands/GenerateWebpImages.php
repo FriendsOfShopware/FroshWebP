4e512f6456dd5a5417cdb7f0c3e9aa816acd2f12
Alex <20777072+jinnoflife@users.noreply.github.com>
2018-12-17 11:13:08 +0100

    Fixed file selection (#12)
    * Fixed file selection
    
    - Changed plain SQL to Repository->findBy
    - Fixed selection of .txt, .pdf, ...
    
    * Tested the changes with many files
    
    The SQL is much faster. avg. 10 seconds with 30.000 elements.
    -> added where to default selection

--- Commands/GenerateWebpImages.php
+++ Commands/GenerateWebpImages.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace ShyimWebP\Commands;
-
 use Shopware\Commands\ShopwareCommand;
 use ShyimWebP\Services\WebpEncoderFactory;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
 /**
  * Class GenerateWebpImages
  * @package ShyimWebP\Commands
@@ -20,7 +17,6 @@ class GenerateWebpImages extends ShopwareCommand
             ->setName('shyim:webp:generate')
             ->setDescription('Generate webp images for all orginal images');
     }
-
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
@@ -31,27 +27,20 @@ class GenerateWebpImages extends ShopwareCommand
         /** @var WebpEncoderFactory $encoderFactory */
         $encoderFactory = $this->container->get('shyim_webp.services.webp_encoder_factory');
         $runnableEncoders = WebpEncoderFactory::onlyRunnable($encoderFactory->getEncoders());
-
         if (empty($runnableEncoders)) {
             $output->writeln('No suitable encoders found');
             return;
         }
-
-        $media = $this->container->get('dbal_connection')->fetchAll('SELECT * FROM s_media');
-
+        $media = $this->container->get('dbal_connection')->fetchAll('SELECT * FROM s_media WHERE type = "IMAGE"');
         $progress = new ProgressBar($output, count($media));
         $progress->start();
-
         foreach ($media as $item) {
             $webpPath = str_replace($item['extension'], 'webp', $item['path']);
-
             try {
                 $im = imagecreatefromstring($this->container->get('shopware_media.media_service')->read($item['path']));
-
                 if ($im === false) {
                     throw new \Exception('Could not load image');
                 }
-
                 imagepalettetotruecolor($im);
                 $content = current($runnableEncoders)->encode($im, 80);
                 imagedestroy($im);
@@ -61,10 +50,8 @@ class GenerateWebpImages extends ShopwareCommand
             } catch (\Throwable $e) {
                 $output->writeln($item['path'] . ' => ' . $e->getMessage());
             }
-
             $progress->advance();
         }
-
         $progress->finish();
         $output->writeln('');
     }
