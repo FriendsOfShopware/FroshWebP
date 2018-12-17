cb72f4b4b4742e96bb4b2ed9694eea3cbfdf8ec2
Alex <20777072+jinnoflife@users.noreply.github.com>
2018-12-17 10:48:17 +0100

    Tested the changes with many files
    The SQL is much faster. avg. 10 seconds with 30.000 elements.
    -> added where to default selection

--- Commands/GenerateWebpImages.php
+++ Commands/GenerateWebpImages.php
@@ -1,16 +1,13 @@
 <?php
-
 namespace ShyimWebP\Commands;
-
 use Shopware\Commands\ShopwareCommand;
-use Shopware\Models\Media\Media;
 use ShyimWebP\Services\WebpEncoderFactory;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
 /**
- * Class GenerateWebpImages.
+ * Class GenerateWebpImages
+ * @package ShyimWebP\Commands
  */
 class GenerateWebpImages extends ShopwareCommand
 {
@@ -20,11 +17,9 @@ class GenerateWebpImages extends ShopwareCommand
             ->setName('shyim:webp:generate')
             ->setDescription('Generate webp images for all orginal images');
     }
-
     /**
-     * @param InputInterface  $input
+     * @param InputInterface $input
      * @param OutputInterface $output
-     *
      * @return int|null|void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -32,43 +27,31 @@ class GenerateWebpImages extends ShopwareCommand
         /** @var WebpEncoderFactory $encoderFactory */
         $encoderFactory = $this->container->get('shyim_webp.services.webp_encoder_factory');
         $runnableEncoders = WebpEncoderFactory::onlyRunnable($encoderFactory->getEncoders());
-
         if (empty($runnableEncoders)) {
             $output->writeln('No suitable encoders found');
-
             return;
         }
-
-        $media = Shopware()->Container()->get('models')->getRepository(Media::class)->findBy([
-            'type' => 'IMAGE',
-        ]);
-
+        $media = $this->container->get('dbal_connection')->fetchAll('SELECT * FROM s_media WHERE type = "IMAGE"');
         $progress = new ProgressBar($output, count($media));
         $progress->start();
-
         foreach ($media as $item) {
-            $webpPath = str_replace($item->getExtension(), 'webp', $item->getPath());
-
+            $webpPath = str_replace($item['extension'], 'webp', $item['path']);
             try {
-                $im = imagecreatefromstring($this->container->get('shopware_media.media_service')->read($item->getPath()));
-
-                if (false === $im) {
+                $im = imagecreatefromstring($this->container->get('shopware_media.media_service')->read($item['path']));
+                if ($im === false) {
                     throw new \Exception('Could not load image');
                 }
-
                 imagepalettetotruecolor($im);
                 $content = current($runnableEncoders)->encode($im, 80);
                 imagedestroy($im);
                 $this->container->get('shopware_media.media_service')->write($webpPath, $content);
             } catch (\Exception $e) {
-                $output->writeln($item->getPath().' => '.$e->getMessage());
+                $output->writeln($item['path'] . ' => ' . $e->getMessage());
             } catch (\Throwable $e) {
-                $output->writeln($item->getPath().' => '.$e->getMessage());
+                $output->writeln($item['path'] . ' => ' . $e->getMessage());
             }
-
             $progress->advance();
         }
-
         $progress->finish();
         $output->writeln('');
     }
