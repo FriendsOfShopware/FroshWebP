f8d8b45893000f93d197046d54aba303588d8a50
Fionera <fionera@fionera.de>
2019-09-30 12:48:36 +0200

    == to === in MediaHydrator

--- Components/MediaHydrator.php
+++ Components/MediaHydrator.php
@@ -109,7 +109,7 @@ class MediaHydrator extends \Shopware\Bundle\StoreFrontBundle\Gateway\DBAL\Hydra
             $media->setHeight((int) $data['__media_height']);
         }
 
-        if ($data['__mediaSettings_create_thumbnails'] && $media->getType() == Media::TYPE_IMAGE) {
+        if ($data['__mediaSettings_create_thumbnails'] && $media->getType() === Media::TYPE_IMAGE) {
             $media->setThumbnails(
                 $this->getMediaThumbnails($data)
             );
