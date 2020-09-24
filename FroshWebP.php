ddf7132d812b5d6a35b44a70f6ec1987e81ef3e0
Soner Sayakci <s.sayakci@shopware.com>
2020-09-24 19:20:40 +0200

    Clear template cache on config change, fixes #73

--- FroshWebP.php
+++ FroshWebP.php
@@ -7,12 +7,20 @@ use Shopware\Components\Plugin;
 use Shopware\Components\Plugin\Context\ActivateContext;
 use Shopware\Components\Plugin\Context\DeactivateContext;
 use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Models\Config\Element;
 
 /**
  * Class FroshWebP
  */
 class FroshWebP extends Plugin
 {
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Shopware_Controllers_Backend_Config_Before_Save_Config_Element' => 'onConfigSave'
+        ];
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -33,5 +41,17 @@ class FroshWebP extends Plugin
     public function deactivate(DeactivateContext $context)
     {
         $context->scheduleClearCache(DeactivateContext::CACHE_LIST_ALL);
+    }
+
+    public function onConfigSave(\Enlight_Event_EventArgs $args)
+    {
+        /** @var Element $element */
+        $element = $args->get('element');
+
+        if ($element->getName() !== 'enableWebPInFrontend') {
+            return;
+        }
+
+        $this->container->get('shopware.cache_manager')->clearTemplateCache();
     }
 }
