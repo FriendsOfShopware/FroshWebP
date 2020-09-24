f78c44d4045d592a71c2c09fcb7d9bf8571e5cde
Soner Sayakci <s.sayakci@shopware.com>
2020-09-24 16:46:09 +0200

    Clear all caches on activate or deactivate

--- FroshWebP.php
+++ FroshWebP.php
@@ -4,6 +4,8 @@ namespace FroshWebP;
 
 use FroshWebP\Services\WebpEncoders\PhpGd;
 use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\ActivateContext;
+use Shopware\Components\Plugin\Context\DeactivateContext;
 use Shopware\Components\Plugin\Context\InstallContext;
 
 /**
@@ -21,5 +23,15 @@ class FroshWebP extends Plugin
         if (!$gd->isRunnable()) {
             $context->scheduleMessage('PHP is not compiled with WebP Support. Please contact your Hosting provider!');
         }
+    }
+
+    public function activate(ActivateContext $context)
+    {
+        $context->scheduleClearCache(ActivateContext::CACHE_LIST_ALL);
+    }
+
+    public function deactivate(DeactivateContext $context)
+    {
+        $context->scheduleClearCache(DeactivateContext::CACHE_LIST_ALL);
     }
 }
